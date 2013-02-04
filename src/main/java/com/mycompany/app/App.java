d224b84ea5d9c84cc09b0af9ec9ed4b870d6666e
Philip Johnson <johnson@hawaii.edu>
2013-02-03 15:22:38 -1000

    Adding source files.

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Boolean bar = new Boolean("true");
+        System.out.println( "Hello World!" );
+    }
+}

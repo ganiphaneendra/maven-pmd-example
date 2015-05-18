7eeae42fb75421faf89f520f70ee4c64865e3459
Ravi Sekhar G <g.ravisekhar@tcs.com>
2015-05-18 18:38:10 +0530

    Removed unused code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,12 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-       try{ Boolean bar = new Boolean("true");
         System.out.println( "Hello World!" );
         System.out.println(":D");
-		} catch(Exception e) {
-		} finally {
-		return;
-		}
+		
     }
 }
